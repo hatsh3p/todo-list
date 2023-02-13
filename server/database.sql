2a1936c3d51335ddb821e039ca85c6fcc22becd3
hatsh3p <sarahamarragy@gmail.com>
2023-02-13 13:57:10 -0800

    Created database schema and sample data.

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,57 @@
-CREATE DATABASE todo_tutorial;
+/* Database schema. */
+CREATE DATABASE todos;
 
-CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+CREATE TABLE Lists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE Items (
+    id SERIAL PRIMARY KEY,
+    list_id INT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (list_id) REFERENCES Lists(id)
+);
+
+/* Sample lists and items. */
+INSERT INTO Lists (name) VALUES ('travel');
+
+INSERT INTO Lists (name) VALUES ('hobbies');
+
+INSERT INTO Lists (name) VALUES ('home');
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Book a flight.'
+FROM Lists
+WHERE name = 'travel';
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Book hotel room.'
+FROM Lists
+WHERE name = 'travel';
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Register for conference.'
+FROM Lists
+WHERE name = 'travel';
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Brainstorm costume ideas.'
+FROM Lists
+WHERE name = 'hobbies';
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Go skating on a paved trail.'
+FROM Lists
+WHERE name = 'hobbies';
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Organize desk wiring.'
+FROM Lists
+WHERE name = 'home';
+
+INSERT INTO Items (list_id, description)
+SELECT id, 'Add accent lighting to office.'
+FROM Lists
+WHERE name = 'home';
